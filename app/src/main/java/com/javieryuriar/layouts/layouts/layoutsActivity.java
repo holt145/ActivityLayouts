79359940c823c93b76bd77c3e825f30c4945ec38
holt145 <holt145gamer@hotmail.com>
2017-08-15 19:22:17 -0500

    first commit

---
+++ app/src/main/java/com/javieryuriar/layouts/layouts/layoutsActivity.java
@@ -0,0 +1,13 @@
+package com.javieryuriar.layouts.layouts;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class layoutsActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.calculadora);
+    }
+}
